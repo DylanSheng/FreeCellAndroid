d6bc75120b960da21fdc2783f90f1822646b2fa1
DylanSheng <shengdiansd@gmail.com>
2017-02-19 13:29:40 -0700

    init

---
+++ app/src/main/java/ca/dylansheng/freecellandroid/FreeCellAndroid.java
@@ -0,0 +1,13 @@
+package ca.dylansheng.freecellandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FreeCellAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_cell_android);
+    }
+}
